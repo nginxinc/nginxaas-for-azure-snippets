8569f40741f307b25d4302a34b523c3bd4409749
sarna <P.Sarna@f5.com>
2024-02-26 22:17:18 -0700

    Add NGINXaaS autoscaling example via Terraform

---
+++ terraform/deployments/with-autoscale/main.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_version = "~> 1.3"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.85"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+module "prerequisites" {
+  source   = "../../prerequisites"
+  location = var.location
+  name     = var.name
+  tags     = var.tags
+}
+
+resource "azurerm_nginx_deployment" "example" {
+  name                = var.name
+  resource_group_name = module.prerequisites.resource_group_name
+  sku                 = var.sku
+  location            = var.location
+  auto_scale_profile {
+    name          = "testProfile"
+    min_capacity  = 10
+    max_capacity  = 30
+  }
+  diagnose_support_enabled = true
+
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [module.prerequisites.managed_identity_id]
+  }
+
+  frontend_public {
+    ip_address = [module.prerequisites.public_ip_address_id]
+  }
+  network_interface {
+    subnet_id = module.prerequisites.subnet_id
+  }
+
+  tags = var.tags
+
+  lifecycle {
+    ignore_changes = [ capacity ]
+  }
+}
+
+resource "azurerm_role_assignment" "example" {
+  scope                = azurerm_nginx_deployment.example.id
+  role_definition_name = "Monitoring Metrics Publisher"
+  principal_id         = module.prerequisites.managed_identity_principal_id
+}
